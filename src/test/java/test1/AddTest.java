87a0c95a90aa0e35c7ab6bd0b5ae3fc3d4c9ab7d
Xi Shen <xis@amazon.com>
2024-02-14 10:33:58 -0800

    commit

--- src/test/java/test1/AddTest.java
+++ src/test/java/test1/AddTest.java
@@ -1,14 +1,14 @@
 package test1;
 
+import org.example.test1.Add;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.example.test1.Add.add;
 
 public class AddTest {
     @Test
     public void test() {
-        Assert.assertEquals(2, add(1, 1));
+        Assert.assertEquals(2, Add.add(1, 1));
     }
 
 }
--- src/test/java/test2/MinusTest.java
+++ src/test/java/test2/MinusTest.java
@@ -1,14 +1,13 @@
 package test2;
 
+import org.example.test2.Minus;
 import org.junit.Assert;
 import org.junit.Test;
-
-import static org.example.test2.Minus.minus;
 
 public class MinusTest {
     @Test
     public void test() {
-        Assert.assertEquals(0, minus(1, 1));
+        Assert.assertEquals(0, Minus.minus(1, 1));
     }
 
 }
